ec05d60bb2f8ce8857372689a75fecdb4bef2963
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-12 13:10:58 +0900

    [Bronze I] Title: 2007년, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,5 +1,22 @@
-days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-m, d = map(int, input().split())
-week = "SUN, MON, TUE, WED, THU, FRI, SAT".split(", ")
-print(week[(sum(days[:m])+d)%7])
-    
+def day_of_month(month):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    else:
+        return 28
+
+def get_days(month, day):
+    days = 0
+    for i in range(1, month):
+        days += day_of_month(i)
+    days += day
+    return days
+
+def day_of_week(days):
+    week = "SUN, MON, TUE, WED, THU, FRI, SAT".split(", ")
+    return week[days%7]
+
+if __name__ == "__main__":
+    month, day = map(int, input().split())
+    print(day_of_week(get_days(month, day)))
