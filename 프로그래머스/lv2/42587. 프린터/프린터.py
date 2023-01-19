e4f0d532f7fcba28c29d198a3a81451dc18f861d
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-19 18:17:36 +0900

    [level 2] Title: 프린터, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -3,6 +3,7 @@ def solution(priorities, location):
     idx = deque([i for i in range(len(priorities))] ) # 인덱스 기억용 리스트
     deq = deque(priorities)
     answer = 0
+    m = max(deq)
     while location in idx:
         n = deq.popleft()
         if len(deq) == 0:
@@ -11,6 +12,7 @@ def solution(priorities, location):
         if n >= max(deq):   # 제일 중요도 높으면
             idx.popleft()  # 인쇄
             answer += 1
+            m = max(deq)
         else:
             deq.append(n)
             idx.rotate(-1)
