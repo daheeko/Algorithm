01f482467ce4dc2bf18b97a63b66e0f0e4d8865c
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-24 22:07:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -3,15 +3,12 @@ input = sys.stdin.readline
 
 mdx =  input().rstrip()
 result = 0
-if '-' not in mdx:  # 10+20 / 20  
-    lst = list(map(int, mdx.split("+")))
-    result = sum(lst)
-else:
-    lst = mdx.split('-')  # ['10', '1+10', '19]
-    for idx, m in enumerate(lst):
-        temp = sum(list(map(int, m.split("+"))))
-        if idx == 0:
-            result += temp
-        else:
-            result -= temp
+
+lst = mdx.split('-')  # ['10', '1+10', '19]
+for idx, m in enumerate(lst):
+    temp = sum(list(map(int, m.split("+"))))
+    if idx == 0:
+        result += temp
+    else:
+        result -= temp
 print(result)
