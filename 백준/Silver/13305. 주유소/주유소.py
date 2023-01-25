ee8ee777ab3f84c627aeea409dbc76d6e90bdf24
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-25 13:09:57 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 45988 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dis = list(map(int, input().split()))
+dist = list(map(int, input().split()))
 price = list(map(int, input().split()))
-temp = []
 min = price[0]
+answer = 0
+
 for i in range(n-1):
     if price[i] <= min:
         min = price[i]
-    temp.append(min)
-result = sum([d*p for d, p in zip(dis, temp)])
-print(result)
+    answer += min*dist[i]
+print(answer)
