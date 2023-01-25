ff4260fe215d7d236fe6f8880373899c778e3ffb
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-25 17:00:14 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,8 @@
+n = int(input())
+dp = [[1] * 10 for _ in range(n+1)]  # [자릿수][끝에 오는 수]
+
+for i in range(2, n+1):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][:j+1])
+
+print(sum(dp[n])%10007)
