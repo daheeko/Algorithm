576a88a336084551d350ba1ed9fa6cda00a4fae8
dahee <90662861+daheeko@users.noreply.github.com>
2023-01-31 17:15:58 +0900

    [Silver II] Title: 제곱수의 합, Time: 4588 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -5,5 +5,7 @@ for i in range(1,n+1):
     for j in range(1, i):
         if j**2 > i:
             break
-        d[i] = min(d[i], 1 + d[i - j**2])
+        # d[i] = min(d[i], 1 + d[i - j**2])
+        if d[i] > 1 + d[i - j**2]:
+            d[i] = 1 + d[i - j**2]  # 제곱수에 대한 값만 체크해서 갱신
 print(d[n])
