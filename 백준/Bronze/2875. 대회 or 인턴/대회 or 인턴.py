3f6f7a129136f0b0d049643d701c169862492055
dahee <90662861+daheeko@users.noreply.github.com>
2023-02-02 16:31:07 +0900

    Update 대회 or 인턴.py

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,19 +1,8 @@
 n, m, k = map(int, input().split())
-result = 0
-g, b = False, False  # 남는 쪽이 True
-while k != 0: 
-    if n > 2*m:
-        g, b = True, False
+for i in range(k):
+    if n >= 2*m:
         n -= 1
-    elif n < 2*m:
-        g, b = False, True
-        m -= 1        
-    else:  # 같아졌으면 일단 여자 빼기
-        n -= 1
-    k -= 1
-if n >= 2*m:
-    result = m
-else:
-    result = n // 2  # 몫
+    else:
+        m -= 1
     
-print(result)
+print(min(n//2, m))
