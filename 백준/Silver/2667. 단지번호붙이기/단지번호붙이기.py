07ba7695bb45fa92a34ba7d564f470e416ee4dbf
dahee <90662861+daheeko@users.noreply.github.com>
2023-02-10 14:40:57 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,42 +1,32 @@
 from collections import deque
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
-def bfs(graph, a, b):
-    n = len(graph)
-    queue = deque()
-    queue.append((a, b))
-    graph[a][b] = 0
-    count = 1
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-                count += 1
-    return count
-
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+graph = [[int(c) for c in input().rstrip()] for _ in range(n)]
+size = []
+cnt = 1
 
-cnt = []
+def bfs(x, y):
+    global cnt
+    q = deque([(x, y)])
+    graph[x][y] = cnt
+    size.append(1)
+    while q:
+        x, y = q.popleft()
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x+dx, y+dy
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1:
+                size[cnt-2] += 1
+                q.append((nx, ny))
+                graph[nx][ny] = cnt
+
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
+            cnt += 1
+            bfs(i, j)
 
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+size.sort()       
+print(len(size))
+print('\n'.join(map(str, size)))
