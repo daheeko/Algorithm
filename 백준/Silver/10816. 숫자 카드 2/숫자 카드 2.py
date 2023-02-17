0738864233c520932ee1b87a41c0c22f881f34e5
dahee <90662861+daheeko@users.noreply.github.com>
2023-02-17 19:54:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 980 ms, Memory: 114492 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,29 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-cards = sorted([*map(int, input().split())])
-M = int(input())
-candidate = [*map(int, input().split())]
-
+n = int(input())
+cards = sorted(list(map(int, input().split())))
+m = int(input())
+nums = list(map(int, input().split()))
 count = {}
+
 for card in cards:
     if card in count:
         count[card] += 1
     else:
         count[card] = 1
 
-def binarySearch(arr, target, start, end):
-    if start > end:
-        return 0
-    
-    mid = (start + end) // 2
-    if arr[mid] == target:
-        return count.get(target)
-    elif arr[mid] < target:
-        return binarySearch(arr, target, mid+1, end)
+for num in nums:
+    if num in count:
+        print(count[num], end = ' ')
     else:
-        return binarySearch(arr, target, start, mid-1)
-    
-for target in candidate:
-    print(binarySearch(cards, target, 0, len(cards)-1), end=" ")
+        print(0, end = ' ')
