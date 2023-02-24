281b36ba4ea17126c0ad545f4defba1a8a9a07b4
dahee <90662861+daheeko@users.noreply.github.com>
2023-02-24 20:21:40 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 580 ms, Memory: 175612 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,7 +4,17 @@ input = sys.stdin.readline
 s = input().rstrip()
 q = int(input())
 
+count = [[0] * 26 for _ in range(len(s))]  # 구간 별로 각 알파벳 개수 셀 배열
+count[0][ord(s[0]) - 97] = 1
+for i in range(1, len(s)):
+    count[i][ord(s[i]) - 97] = 1  # 문자열 (i, i번째 알파벳) 위치 1로 초기화
+    for j in range(26):
+        count[i][j] += count[i-1][j]  # 직전까지의 알파벳 개수 반영해주기
+
 for _ in range(q):
-    p = input().split()
-    sub = s[int(p[1]) : int(p[2])+1]
-    print(sub.count(p[0]))
+    a, l, r = input().split()
+    l, r = map(int, [l, r])
+    if l > 0:
+        print(count[r][ord(a) - 97] - count[l-1][ord(a) - 97])
+    else:
+        print(count[r][ord(a) - 97])
