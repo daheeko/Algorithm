71a57f9356e0e4720b4e8bc734766ba49f9c8850
dahee <90662861+daheeko@users.noreply.github.com>
2023-03-03 23:40:40 +0900

    [Silver III] Title: 회전하는 큐, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,28 +1,8 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-nums = list(map(int, input().split()))
-deq = deque([i for i in range(1, n+1)])
-result = 0
-
-for num in nums:
-    idx = deq.index(num)
-    front = idx
-    back = len(deq) - idx
-    while True:
-        if deq[0] == num:  #
-            deq.popleft()
-            break
-        else:
-            if front <= back:  # 왼쪽 한 칸, 즉 앞에 애들을 뒤로
-                deq.rotate(-1)
-                front -= 1
-                back += 1
-            else:
-                deq.rotate(1)
-                front += 1
-                back -= 1
-            result += 1
-print(result)
+N, M = map(int, input().split())
+Q = [*range(1, N+1)]
+idx, cnt=0, 0
+for i in list(map(int, input().split())):
+    cnt += min(abs(idx-Q.index(i)), len(Q)-abs(idx-Q.index(i)))
+    idx = Q.index(i)
+    del Q[Q.index(i)]
+print(cnt)
